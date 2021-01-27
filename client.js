100988bc3a53637b68d8032ffdbfc78b0d47b7bb
Josh Gulledge <joshcalebgulledge@gmail.com>
2021-01-26 20:51:17 -0600

    bonus calculator

--- client.js
+++ client.js
@@ -3,32 +3,32 @@ const employees = [
     name: 'Atticus',
     employeeNumber: '2405',
     annualSalary: '47000',
-    reviewRating: 3
+    reviewRating: 3,
   },
   {
     name: 'Jem',
     employeeNumber: '62347',
     annualSalary: '63500',
-    reviewRating: 4
+    reviewRating: 4,
   },
   {
     name: 'Scout',
     employeeNumber: '6243',
     annualSalary: '74750',
-    reviewRating: 5
+    reviewRating: 5,
   },
   {
     name: 'Robert',
     employeeNumber: '26835',
     annualSalary: '66000',
-    reviewRating: 1
+    reviewRating: 1,
   },
   {
     name: 'Mayella',
     employeeNumber: '89068',
     annualSalary: '35000',
-    reviewRating: 1
-  }
+    reviewRating: 1,
+  },
 ];
 
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
@@ -40,50 +40,58 @@ const employees = [
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
-console.log( employees );
+console.log(employees);
 
-function calculateBonus (employee) {
+function calculateBonus(employee) {
+  const newEmployeeObj = {};
+  newEmployeeObj.name = employee.name;
 
-  const newObj = {
-    name: employee.name, 
-    bonusPercentage: 0, 
-    totalCompensation: 0, 
-    totalBonus: 0, 
-  };
-
+  // if rating of 2 or below no bonus
   if (employee.reviewRating <= 2) {
-    // no bonus
-    newObj.totalBonus = 0;
-  };
+    newEmployeeObj.bonusPercentage = 0;
+  }
+  // rating of 3 get 4%
   if (employee.reviewRating === 3) {
-    // bonus 4% base annual income
-    newObj.totalBonus = employee.annualSalary * .04;
-  };
+    newEmployeeObj.bonusPercentage = 0.04;
+  }
+  //  rating of 4 get 6%
   if (employee.reviewRating === 4) {
-    // bonus 6% base annual income
-    newObj.totalBonus = employee.annualSalary * .06;
+    newEmployeeObj.bonusPercentage = 0.06;
   }
+  // rating of 5 get 10%
   if (employee.reviewRating === 5) {
-    // bonus 10% base annual income
-    newObj.totalBonus = employee.annualSalary * .1;
+    newEmployeeObj.bonusPercentage = 0.1;
   }
 
-  // check for more than 4 numbers
-  if (employee.employeeNumber.length <= 4) {
-    console.log(employee.employeeNumber)
-    console.log(newObj.totalBonus)
-    newObj.totalBonus *= .05;
-    console.log(newObj.totalBonus)
+  // emplyNumber === 4 digits long additional 5%
+  if (employee.employeeNumber.length === 4) {
+    newEmployeeObj.bonusPercentage += 0.05;
+  }
 
-  };
+  // annualIncome > 65,000 bonus down to 1%
+  if ((+employee.annualSalary > 65, 000)) {
+    newEmployeeObj.bonusPercentage = 0.01;
+  }
 
-  // 
+  // no bonus is above 13% or below0%
+  if (newEmployeeObj.bonusPercentage >= 0.13) {
+    newEmployeeObj.bonusPercentage = 0.12;
+  }
+  if (newEmployeeObj.bonusPercentage < 0) {
+    newEmployeeObj.bonusPercentage = 0;
+  }
 
-  return newObj;
+  newEmployeeObj.totalBonus =
+    employee.annualSalary * newEmployeeObj.bonusPercentage;
 
+  newEmployeeObj.totalCompensation =
+    Number(employee.annualSalary) + newEmployeeObj.totalBonus;
+
+  return newEmployeeObj;
 }
+
+//
 
 for (let i = 0; i < employees.length; i++) {
   console.log(calculateBonus(employees[i]));
-};
-
+}
